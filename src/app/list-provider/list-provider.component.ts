46e4b245ce50de66dabe3a96f1d57316c9f56eb4
Kounouz Hlimi <Kounouz Hlimi@cynapsys.local>
2021-08-05 10:58:30 +0200

    commit

---
+++ src/app/list-provider/list-provider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-provider',
+  templateUrl: './list-provider.component.html',
+  styleUrls: ['./list-provider.component.css']
+})
+export class ListProviderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
